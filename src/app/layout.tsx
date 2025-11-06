12425fee1f17da689272b00107bd097dc6a94b6e
Vinicio Velastegui <vinicio.velasteguis@gmail.com>
2025-11-06 12:18:21 +0000

    quiero añadir analytic de vercel segun la pagina las intrucciones son: 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { cn } from '@/lib/utils';
+import { Analytics } from "@vercel/analytics/next"
 
 
 export const metadata: Metadata = {
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={cn("font-body antialiased", "min-h-screen bg-background")}>
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
