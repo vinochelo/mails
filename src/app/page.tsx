aa99b9ea26e3f822648344464c9d9dddeb5833ef
Vinicio Velastegui <vinicio.velasteguis@gmail.com>
2026-02-03 13:12:57 +0000

    el error persiste revisa por favor When to upgrade your application You

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,6 +85,7 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
+    // Asegurarse de que el scroll suba al inicio en cada cambio de paso
     window.scrollTo({ top: 0, behavior: "smooth" });
   }, [step]);
 
